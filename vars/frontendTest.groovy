fb101151eaca5a88693e240dcf6bd6e73f2cbcd6
isebastienstore <ahmanydanbaibe@gmail.com>
2024-06-30 11:16:22 +0000

    clean, install tool, npm install, compile, checkstyle

--- vars/frontendTest.groovy
+++ vars/frontendTest.groovy
@@ -1,13 +1,3 @@
 def call(){
-        bat '''
-    @echo off
-    setlocal enabledelayedexpansion
-    set FOUND=0
-    for /r src\\main\\webapp\\app %%i in (*.spec.ts) do (
-        echo %%i
-        set FOUND=1
-    )
-    if !FOUND! == 0 echo "No test files found"
-    '''
-    bat 'npm test'
+    bat 'npm test -- --passWithNoTests'
 }
