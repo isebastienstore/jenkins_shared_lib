172247174aa33d77f45f6f85a808047438e29c22
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-01 10:17:16 +0000

    clean, install tool, npm install, compile, checkstyle

--- vars/mvnIntegrationTest.groovy
+++ vars/mvnIntegrationTest.groovy
@@ -1,3 +1,9 @@
 def call(){
-    bat 'mvn -ntp verify -P-webapp'
+    try {
+        bat 'mvn -ntp verify -P-webapp'
+    } catch (err) {
+        throw err
+    } finally {
+        junit '**/target/surefire-reports/TEST-*.xml,**/target/failsafe-reports/TEST-*.xml'
+    }
 }
