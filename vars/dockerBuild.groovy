76020515689d4e726e9cb4894c602a60d10b58f7
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-22 11:46:02 +0000

    clean, install tool, npm install, compile, checkstyle

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -7,10 +7,24 @@
 //     """
 // }
 
-def call(String aws_account_id, String region, String ecr_repoName){
+//def call(String aws_account_id, String region, String ecr_repoName){
     
-    bat """
-     docker build -t ${ecr_repoName} .
-     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//    bat """
+//     docker build -t ${ecr_repoName} .
+//     docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//    """
+//}
+
+def call(String project, String ImageTag, String hubUser, boolean isArm64 = false) {
+    def buildCommand = isArm64 ? "npm run java:docker:arm64" : "npm run java:docker"
+
+    sh """
+      ${buildCommand}
+
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+
+      # Démarrage des services Docker
+      #docker compose -f src/main/docker/app.yml up -d
     """
 }
