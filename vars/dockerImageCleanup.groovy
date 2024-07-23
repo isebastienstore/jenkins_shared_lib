8384a1070ae6405d398ebf92d808faf3ce74bba6
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-23 11:44:48 +0000

    update config

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,10 +1,24 @@
- def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
-     bat """
-      docker rmi ${hubUser}/${project}:${ImageTag}
-      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
-     """
- }
+//     bat """
+//      docker rmi ${hubUser}/${project}:${ImageTag}
+//      docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+// }
+
+ def call(String project, String ImageTag, String hubUser){
+    bat """
+        powershell -Command "
+            if (docker images -q ${hubUser}/${project}:${ImageTag}) {
+                docker rmi ${hubUser}/${project}:${ImageTag}
+            }
+            if (docker images -q ${hubUser}/${project}:latest) {
+                docker rmi ${hubUser}/${project}:latest
+            }
+        "
+    """
+}
+
 
 
 //def call(String aws_account_id, String region, String ecr_repoName){
