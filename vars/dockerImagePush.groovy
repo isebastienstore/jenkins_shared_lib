87ef097d47b05dabc8d9452ddc6588aa39e1a8a4
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-24 09:59:43 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,3 +1,32 @@
+
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    bat """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
+}
+
+//Version valide pour docker hub
+
+//def call(String project, String ImageTag, String hubUser){
+//    withCredentials([string(credentialsId: 'docker-hub-password', variable: 'PASS')]) {
+//        bat """
+//            powershell -Command "\$pass = \$env:PASS; Set-Content -Path password.txt -Value \$pass; Get-Content -Path password.txt | docker login -u ${hubUser} --password-stdin"
+//            docker image push ${hubUser}/${project}:${ImageTag}
+//            docker image push ${hubUser}/${project}:latest
+//            del password.txt
+//        """
+//    }
+//}
+
+
+
+
+
+//peut etre utile
+
 // def call(String project, String ImageTag, String hubUser){
 //     withCredentials([usernamePassword(
 //             credentialsId: "docker",
@@ -9,24 +38,3 @@
 //     bat "docker image push ${hubUser}/${project}:${ImageTag}"
 //     bat "docker image push ${hubUser}/${project}:latest"   
 // }
-
-//def call(String aws_account_id, String region, String ecr_repoName){
-//    
-//    bat """
-//     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-//     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//    """
-//}
-
-
-def call(String project, String ImageTag, String hubUser){
-    withCredentials([string(credentialsId: 'docker-hub-password', variable: 'PASS')]) {
-        bat """
-            powershell -Command "\$pass = \$env:PASS; Set-Content -Path password.txt -Value \$pass; Get-Content -Path password.txt | docker login -u ${hubUser} --password-stdin"
-            docker image push ${hubUser}/${project}:${ImageTag}
-            docker image push ${hubUser}/${project}:latest
-            del password.txt
-        """
-    }
-}
-
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,15 +1,15 @@
- def call(String project, String ImageTag, String hubUser){
-    
-     bat """   
-      trivy image ${hubUser}/${project}:latest > scan.txt
-      type scan.txt
-     """
- }
-
-//def call(String aws_account_id, String region, String ecr_repoName){
+// def call(String project, String ImageTag, String hubUser){
 //    
-//    bat """
-//    trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
-//    cat scan.txt
-//    """
-//}
+//     bat """   
+//         trivy image ${hubUser}/${project}:latest > scan.txt
+//         type scan.txt
+//     """
+// }
+
+def call(String aws_account_id, String region, String ecr_repoName){
+    
+    bat """
+        trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
+        type scan.txt
+    """
+}
