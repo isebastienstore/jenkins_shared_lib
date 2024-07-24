28ea11381e19c4f7e2763df51ab7117e11262dbb
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-24 12:36:29 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,12 +1,11 @@
 
-
 def call(String aws_account_id, String region, String ecr_repoName){
-    
     bat """
-     (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+        powershell -Command "\$ecrLoginPassword = aws ecr get-login-password --region ${region}; docker login --username AWS --password \$ecrLoginPassword ${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
+        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
+
 
 //Version valide pour docker hub
 
