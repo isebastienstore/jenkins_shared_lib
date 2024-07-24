438c3aa80f1ad0bb829a014843ed00b620ba1003
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-24 10:05:54 +0000

    update config

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,15 +1,15 @@
- def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
-     bat """
-      docker rmi ${hubUser}/${project} 
-      docker rmi ${hubUser}/${project}:${ImageTag}
-     """
- }
+//     bat """
+//      docker rmi ${hubUser}/${project} 
+//      docker rmi ${hubUser}/${project}:${ImageTag}
+//     """
+// }
 
 
-//def call(String aws_account_id, String region, String ecr_repoName){
+def call(String aws_account_id, String region, String ecr_repoName){
     
-//    bat """
-//     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//    """
-//}
+    bat """
+     docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+    """
+}
