d6af9b464f5126ce0de65dc473070b183795f751
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-24 12:14:48 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -4,7 +4,7 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     bat """
      aws sts get-caller-identity
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+     (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
 }
