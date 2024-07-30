d237509c5809f00829066189ef2fb849f7e84962
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-30 17:31:37 +0000

    Addind file to repo

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-        bat 'mvn -Pprod org.sonarsource.scanner.maven:sonar-maven-plugin:3.11.0.3922:sonar'
+        sh './mvnw -Pprod org.sonarsource.scanner.maven:sonar-maven-plugin:3.11.0.3922:sonar'
     }
 }
