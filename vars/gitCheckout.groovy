f51922df329bcdf1cdcc582935870861684d35fc
isebastienstore <ahmanydanbaibe@gmail.com>
2024-07-30 17:07:53 +0000

    Addind file to repo

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,3 +1,8 @@
-def call() {
-    checkout scm
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
   }
