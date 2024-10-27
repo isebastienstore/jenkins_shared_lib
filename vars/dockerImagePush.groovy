b2dd9cf5ee94002e34b87965968564cb1e71d7cf
isebastienstore <sabastiandanbaibe@esp.sn>
2024-10-27 23:16:07 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,12 +1,12 @@
 def call(String aws_account_id, String region, String ecr_repoName) {
     withCredentials([
-        string(credentialsId: 'aws_access_key', variable: 'AWS_ACCESS_KEY_ID'),
-        string(credentialsId: 'aws_secret_key', variable: 'AWS_SECRET_ACCESS_KEY')
+        string(credentialsId: 'AWS_ACCESS_KEY', variable: 'AWS_ACCESS_KEY_ID'),
+        string(credentialsId: 'AWS_SECRET_KEY', variable: 'AWS_SECRET_ACCESS_KEY')
     ]) {
         // Configure AWS CLI avec les clés d'accès
         sh """
-            aws configure set aws_access_key_id ${AWS_ACCESS_KEY}
-            aws configure set aws_secret_access_key ${AWS_SECRET_KEY}
+            aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+            aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
             aws configure set region ${region}
 
             aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
@@ -14,37 +14,3 @@ def call(String aws_account_id, String region, String ecr_repoName) {
         """
     }
 }
-
-
-
-
-
-
-//def call(String project, String ImageTag, String hubUser){
-//    withCredentials([string(credentialsId: 'docker-hub-password', variable: 'PASS')]) {
-//        bat """
-//            powershell -Command "\$pass = \$env:PASS; Set-Content -Path password.txt -Value \$pass; Get-Content -Path password.txt | docker login -u ${hubUser} --password-stdin"
-//            docker image push ${hubUser}/${project}:${ImageTag}
-//            docker image push ${hubUser}/${project}:latest
-//            del password.txt
-//        """
-//    }
-//}
-
-
-
-
-
-//peut etre utile
-
-// def call(String project, String ImageTag, String hubUser){
-//     withCredentials([usernamePassword(
-//             credentialsId: "docker",
-//             usernameVariable: "USER",
-//             passwordVariable: "PASS"
-//     )]) {
-//         bat "docker login -u '$USER' -p '$PASS'"
-//     }
-//     bat "docker image push ${hubUser}/${project}:${ImageTag}"
-//     bat "docker image push ${hubUser}/${project}:latest"   
-// }
