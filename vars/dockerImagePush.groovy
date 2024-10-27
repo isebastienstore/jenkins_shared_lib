7adb1bd8aa26f598183098a006deadc69cc9cfd5
isebastienstore <sabastiandanbaibe@esp.sn>
2024-10-27 23:05:19 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,11 +1,20 @@
-def call(String aws_account_id, String region, String ecr_repoName){
-    
-    sh """
-        aws --version
-        aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-        docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+def call(String aws_account_id, String region, String ecr_repoName) {
+    withCredentials([
+        string(credentialsId: 'aws_access_key', variable: 'AWS_ACCESS_KEY_ID'),
+        string(credentialsId: 'aws_secret_key', variable: 'AWS_SECRET_ACCESS_KEY')
+    ]) {
+        // Configure AWS CLI avec les clés d'accès
+        sh """
+            aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+            aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+            aws configure set region ${region}
+
+            aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+            docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+        """
+    }
 }
+
 
 
 
