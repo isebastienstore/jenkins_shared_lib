e8244e9670892ee8e4dba7395bc60945b5097b22
isebastienstore <sabastiandanbaibe@esp.sn>
2024-10-27 23:08:59 +0000

    update config

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -5,8 +5,8 @@ def call(String aws_account_id, String region, String ecr_repoName) {
     ]) {
         // Configure AWS CLI avec les clés d'accès
         sh """
-            aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-            aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+            aws configure set aws_access_key_id ${AWS_ACCESS_KEY}
+            aws configure set aws_secret_access_key ${AWS_SECRET_KEY}
             aws configure set region ${region}
 
             aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
