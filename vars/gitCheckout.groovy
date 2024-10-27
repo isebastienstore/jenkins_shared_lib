f4225481f97c127067cc01ccb8a884d08f940e31
isebastienstore <sabastiandanbaibe@esp.sn>
2024-10-27 22:11:28 +0000

    update config

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,10 +1,10 @@
 def call(Map stageParams) {
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [[
-            url: stageParams.url,
-            credentialsId: stageParams.credentialsId // Ajoutez ceci
-        ]]
-    ])
+checkout([
+    $class: 'GitSCM',
+    branches: [[name: stageParams.branch]],
+    userRemoteConfigs: [[
+        url: stageParams.url,
+        credentialsId: stageParams.credentialsId
+    ]]
+])
 }
