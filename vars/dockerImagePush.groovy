a1a0ab1e2ea837d2ed70b36e95488cdc458fa65b
isebastienstore <sabastiandanbaibe@esp.sn>
2025-07-21 10:44:49 +0000

    update docker push in jenkins shared library

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -5,12 +5,15 @@ def call(String aws_account_id, String region, String ecr_repoName) {
     ]) {
         // Configure AWS CLI avec les clés d'accès
         sh """
-            aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
-            aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-            aws configure set region ${region}
-
+            set +x
+            export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+            export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+            export AWS_DEFAULT_REGION=${region}
             aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+            set -x
+
             docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
         """
+
     }
 }
